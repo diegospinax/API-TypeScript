04faa7b7d2060bc9103698ca92a5652a0c078bab
Diego Ospina <oadiexiii@gmail.com>
2025-07-30 12:33:44 -0500

    Third Class Commit

---
+++ 3/src/infrastructure/config/environment-vars.ts
@@ -0,0 +1,55 @@
+import * as joi from "joi";
+import "dotenv/config";
+
+export type Environments = {
+  PORT: number;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USER: string;
+  DB_NAME: string;
+  DB_PASSWORD: string;
+};
+
+type ValidationEnvironments = {
+  error: joi.ValidationError | undefined;
+  value: Environments;
+};
+
+const validateEnvironments = (
+  vars: NodeJS.ProcessEnv
+): ValidationEnvironments => {
+  const envScheme = joi
+    .object({
+      PORT: joi.number().required(),
+      DB_HOST: joi.string().required(),
+      DB_PORT: joi.number().default(3306),
+      DB_USER: joi.string().required(),
+      DB_NAME: joi.string().required(),
+      DB_PASSWORD: joi.string().allow("").optional(),
+    })
+    .unknown(true);
+
+  const { error, value } = envScheme.validate(vars);
+  return { error, value };
+};
+
+const loadEnvironments = (): Environments => {
+  const result = validateEnvironments(process.env);
+
+  if (result.error) throw new Error("Error validating environments variables.");
+
+  const value = result.value;
+
+  return {
+    PORT: value.PORT,
+    DB_HOST: value.DB_HOST,
+    DB_PORT: value.DB_PORT,
+    DB_USER: value.DB_USER,
+    DB_NAME: value.DB_NAME,
+    DB_PASSWORD: value.DB_PASSWORD,
+  };
+};
+
+const environments = loadEnvironments();
+
+export default environments;
